917f823335164202fe9a9d59a62b338988e328dc
Li Bo <570446979@qq.com>
2019-06-21 15:21:50 +0800

    first commit

---
+++ src/pages/City/City.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list :cities="cities" :hot="hot" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CityHeader from './components/header'
+import CitySearch from './components/search'
+import CityList from './components/list'
+import CityAlphabet from './components/alphabet'
+export default {
+  name: 'City',
+  data () {
+    return {
+      cities: {},
+      hot: [],
+      letter: ''
+    }
+  },
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json').then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hot = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <home-header></home-header>
+        <home-swiper :list="swiperList"></home-swiper>
+        <home-icons :list="iconList"></home-icons>
+        <home-recommend :list="recommendList"></home-recommend>
+        <home-weekend :list="weekendList"></home-weekend>
+    </div>
+</template>
+<script>
+import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
+import HomeRecommend from './components/recommend'
+import HomeWeekend from './components/weekend'
+import axios from 'axios'
+import { mapState } from 'vuex'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      lastCity: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  }
+}
+</script>
+<style>
+
+</style>
